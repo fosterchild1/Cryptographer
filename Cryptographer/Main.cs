8e9ceb0afa52e6d9eaee13d5cc05629ee196b57c
fosterchild1 <crannberriesa@gmail.com>
2025-07-29 12:19:02 +0300

    terminal arguments

--- Cryptographer/Main.cs
+++ Cryptographer/Main.cs
@@ -157,12 +157,25 @@ class Program
 
     static void Main(string[] args)
     {
+
+        Dictionary<string, string?> argDict = args.Select(a => a.Split('='))
+                     .ToDictionary(a => a[0], a => a.Length == 2 ? a[1] : null);
+
+        if (args.Length > 0)
+            Constants.Set(argDict);
+
         Console.WriteLine("cryptographer is a tool for decrypting text");
 
-        string input = ProjUtils.GetInput();
+        string? input = "";
+        if (!argDict.TryGetValue("input", out input))
+            input = ProjUtils.GetInput();
         Console.Clear();
-        maxDepth = ProjUtils.GetDepth();
+
+        if (!byte.TryParse(argDict.GetValueOrDefault("maxdepth"), out maxDepth))
+            maxDepth = ProjUtils.GetDepth();
         Console.Clear();
+
+
         Console.WriteLine("Working...");
 
         DecryptionNode root = GetDecrypted(input, 1);
--- Cryptographer/Utils/Constants.cs
+++ Cryptographer/Utils/Constants.cs
@@ -11,5 +11,14 @@ namespace Cryptographer.Utils
         public static float scorePrintThreshold = 10;
 
         public static float scoreBreakSearchThreshold = 100;
+
+        public static void Set(Dictionary<string, string?> args)
+        {
+            if (float.TryParse(args.GetValueOrDefault("score_print"), out float score_print))
+                scorePrintThreshold = score_print;
+
+            if (float.TryParse(args.GetValueOrDefault("score_breaksearch"), out float score_breaksearch))
+                scoreBreakSearchThreshold = score_breaksearch;
+        }
     }
 }
