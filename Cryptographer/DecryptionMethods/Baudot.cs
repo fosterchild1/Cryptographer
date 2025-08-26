4af5b0312194f02d9af42f72b6ccb6f3be5e7e82
fosterchild1 <crannberriesa@gmail.com>
2025-08-26 16:23:57 +0300

    no magic

--- Cryptographer/DecryptionMethods/Baudot.cs
+++ Cryptographer/DecryptionMethods/Baudot.cs
@@ -10,6 +10,9 @@ namespace Cryptographer.DecryptionMethods
 
         private Dictionary<string, char> BaudotDictionary = MethodDictionaries.Baudot;
 
+        private readonly string SWITCH_TO_FIGURES = "11011";
+        private readonly string SWITCH_TO_LETTERS = "111111";
+
         public List<string> Decrypt(string input, List<KeyValuePair<char, int>> analysis)
         {
             StringBuilder output = new();
@@ -20,8 +23,8 @@ namespace Cryptographer.DecryptionMethods
                 // baudot is stricly 5 characters
                 if (s.Length != 5) return new();
 
-                if (s == "11011") useFigures = true;
-                if (s == "11111") useFigures = false;
+                if (s == SWITCH_TO_FIGURES) useFigures = true;
+                if (s == SWITCH_TO_LETTERS) useFigures = false;
 
                 if (!BaudotDictionary.TryGetValue(useFigures ? s + "_" : s, out char c)) return new();
                 output.Append(c);
